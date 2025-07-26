bc1538ba69d38a5174161a2f72ec1052c605d404
lahotimohit <engtechno25@gmail.com>
2025-07-26 12:47:07 +0530

    Created models for transaction

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    uuid=models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4, unique=True)
+    created_at=models.DateField(auto_now=True)
+    updated_at=models.DateField(auto_now_add=True)
+
+class Transaction(BaseModel):
+    description=models.CharField(max_length=100)
+    amount=models.FloatField()
+
+    class Meta:
+        ordering = ("description",)
+
+    def is_negative(self):
+        return self.amount < 0
