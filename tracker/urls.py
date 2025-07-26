8cf327ec5c0932e3b6457e97e767dd0f160083b9
lahotimohit <engtechno25@gmail.com>
2025-07-26 16:33:56 +0530

    Complete expense tracker project

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from tracker import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index),
+    path("delete/<uuid>", views.delete, name='delete-expense')
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db.models import Sum
+from django.contrib import messages
+from tracker.models import Transaction
 
 def index(request):
-    return render(request, "index.html")
+    if request.method == "POST":
+        description=request.POST.get("description")
+        amount=request.POST.get("amount")
+        if description is None or description=="":
+            messages.info(request, "Please add description...")
+            return redirect("/")
+
+        try:
+            amount=float(amount)
+        except:
+            messages.info(request, "Amount should be number...")
+            return redirect("/")
+
+        Transaction.objects.create(description=description, amount=amount)
+        print("transaction created....")
+        return redirect("/")
+    context = {
+        "transactions": Transaction.objects.all(),
+        "balance": Transaction.objects.all().aggregate(amount=Sum('amount'))['amount'] or 0,
+        "income": Transaction.objects.filter(amount__gt=0).aggregate(income=Sum('amount'))['income'] or 0,
+        "expense": Transaction.objects.filter(amount__lte=0).aggregate(expense=Sum('amount'))['expense'] or 0
+        }
+    return render(request, "index.html", context=context)
+
+def delete(request, uuid):
+    expense=Transaction.objects.filter(uuid=uuid).first()
+    expense.delete()
+    return redirect("/")
